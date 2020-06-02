6790a79a1b7de6d5611d09f301f0fe39320633ce
Fernanda Vergara <fernandavmelin@gmail.com>
2020-06-02 14:29:38 -0400

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div id="color1">
+      <h2>Random Gif Cat</h2>
+    </div>
+
+    <div class="parameters" id="color2">
+
+      <div>
+        <label for="gif-titulo">Título:</label>
+        <input type="text" id="gif-titulo" v-model="title"/>
+      </div>
+
+      <div>
+        <label for="gif-filtro">Filtro:</label>
+        <select type="text" id="gif-filtro" v-model="filter">
+          <option value="blur">Blur</option>
+          <option value="mono">Mono</option>
+          <option value="sepia">Sepia</option>
+          <option value="negative">Negative</option>
+          <option value="paint">Paint</option>
+          <option value="pixel">Pixel</option>
+        </select>
+      </div>
+
+      <div>
+        <label for="gif-color">Color:</label>
+        <select type="text" id="gif-color" v-model="color">
+          <option value="red">Rojo</option>
+          <option value="orange">Naranja</option>
+          <option value="yellow">Amarillo</option>
+          <option value="green">Verde</option>
+          <option value="ightskyblue">Celeste</option>
+          <option value="blue">Azul</option>
+          <option value="blueviolet">Violeta</option>
+        </select>
+      </div>
+      
+      <div>
+        <label for="gif-tamaño">Tamaño:</label>
+        <input type="text" id="gif-tamaño" v-model="size">
+      </div>
+      
+    </div>
+
+    <div id="color1">
+      <button>Obtener mi gatito</button><br>
+      <img :src="src" alt="gatito :3">
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      title: '',
+      filter: '',
+      color: '',
+      size: '',
+    }
+  },
+  computed: {
+    src(){
+      let title = this.title;
+      let filter = this.filter;
+      let color = this.color;
+      let size = this.size;
+      return `https://cataas.com/cat/gif/says/${title}?filter=${filter}&color=${color}&size=${size}`
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
 }
+.parameters{
+  display: flex;
+  flex-direction: column;
+}
+
+#color1{
+  background-color: #f08080;
+}
+
+#color2{
+  background-color: #add8e6;
+}
+
 </style>
